fcf39faa230fc43d3998b466f768ea455ed5172c
THAINA OLIVEIRA ARAUJO <thaina.oaraujo1@senacsp.edu.br>
2022-06-06 11:31:22 -0300

    First Commit

---
+++ funcao.js
@@ -0,0 +1,22 @@
+/* function maior (n1, n2){
+    if(n1 > n2){
+        console.log(`maior ${n1}`)
+    } else {
+        console.log(`maior ${n2}`)
+    }
+}
+maior(30,15)  
+
+
+
+
+
+
+var fah = prompt ("Temperatura em graus Fahrenheit:")
+ 
+function fahrenheit(fah){
+    return ((fah - 32) * 5/9);
+}
+
+alert(fahrenheit(fah, 5/9)) */
+
---
+++ readme.md
@@ -0,0 +1,8 @@
+## Exercicio de lógica JavaScript
+
+*Funções
+*Função sem paramêtro
+*Função com paramêtro 
+*Função com paramêtro e retorno
+*Função anônima 
+* Arrow Function 
